c5873495df17d402dd5cdc476761f93ed9de2bd2
Alisson Matias <alissonmds00@gmaill..com>
2024-11-14 00:43:32 -0300

    feat: adicionados municipios e estados

--- src/main/java/com/alissonmds/LostPets/domain/models/usuario/Usuario.java
+++ src/main/java/com/alissonmds/LostPets/domain/models/usuario/Usuario.java
@@ -6,8 +6,6 @@ import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Collection;
 import java.util.List;
--- src/main/resources/db/migration/V1.create-table-usuario.sql
+++
@@ -1,7 +0,0 @@
-create table usuarios(
-    id bigint not null auto_increwent,
-    login varchar(50) UNIQUE,
-    senha varchar(100),
-    ativo smallint,
-    primary key (id), UNIQUE(login)
-)
