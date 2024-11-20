e67b53dbdcc9eac26bf4bc749886a512b9dfe1a4
Alisson Matias <alissonmds00@gmaill..com>
2024-11-20 19:27:54 -0300

    refactor: melhorias no funcionamento no filtro dos municípios.

--- src/main/java/com/alissonmds/LostPets/controller/EnderecoController.java
+++ src/main/java/com/alissonmds/LostPets/controller/EnderecoController.java
@@ -1,12 +1,14 @@
 package com.alissonmds.LostPets.controller;
 
 import com.alissonmds.LostPets.domain.models.endereco.Estado;
+import com.alissonmds.LostPets.domain.models.endereco.Municipio;
 import com.alissonmds.LostPets.repository.EstadoRepository;
 import com.alissonmds.LostPets.repository.MunicipioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -28,5 +30,13 @@ public class EnderecoController {
                 .map(Estado::getNome)
                 .collect(Collectors.toList());
         return ResponseEntity.ok(estados);
+    }
+
+    @GetMapping("/{nomeEstado}")
+    public ResponseEntity<List<String>> listarMunicipios(@PathVariable String nomeEstado) {
+        List<String> municipios = municipioRepository.findAllByNomeEstado(nomeEstado).stream()
+                .map(Municipio::getNome)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(municipios);
     }
 }
--- src/main/java/com/alissonmds/LostPets/repository/MunicipioRepository.java
+++ src/main/java/com/alissonmds/LostPets/repository/MunicipioRepository.java
@@ -2,6 +2,11 @@ package com.alissonmds.LostPets.repository;
 
 import com.alissonmds.LostPets.domain.models.endereco.Municipio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MunicipioRepository extends JpaRepository<Municipio, Long> {
+    @Query("SELECT m FROM Municipio m JOIN Estado e ON m.uf = e.uf WHERE e.nome = :nome")
+    List<Municipio> findAllByNomeEstado(String nome);
 }
