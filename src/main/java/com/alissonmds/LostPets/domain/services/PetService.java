643a3a2b198e10e812b6060520b4b73606593ed9
Alisson Matias <alissonmds00@gmaill..com>
2024-12-10 13:37:36 -0300

    refactor: apenas autores ou admins podem remover um post

--- src/main/java/com/alissonmds/LostPets/domain/services/PetService.java
+++ src/main/java/com/alissonmds/LostPets/domain/services/PetService.java
@@ -10,7 +10,6 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 @Service
