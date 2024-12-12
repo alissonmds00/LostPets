235bd29c2e7fbc854595ab6fd7de2dabc50f97f6
Alisson Matias <alissonmds00@gmaill..com>
2024-12-12 01:23:54 -0300

    feat: documentação com swagger ui

--- src/main/java/com/alissonmds/LostPets/controller/EnderecoController.java
+++ src/main/java/com/alissonmds/LostPets/controller/EnderecoController.java
@@ -5,7 +5,6 @@ import com.alissonmds.LostPets.domain.models.endereco.Municipio;
 import com.alissonmds.LostPets.repository.MunicipioRepository;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
